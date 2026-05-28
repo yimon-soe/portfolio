1f432fdadb2f1ffc5c0b449b36586d0f0e157ef4
yimon <yimonsoe.yms@icloud.com>
2026-05-29 11:20:53 +1200

    fixed domain issue for github pages by switching to hash router and updating homepage url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';import Navbar from './components/Navbar';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import './App.css';
